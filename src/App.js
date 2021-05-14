03aec80f03d544627512dd0f9c2410dda7480f53
daiki130 <daikibaseball130@gmail.com>
2021-05-14 20:40:00 +0900

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header-container">
+        <h1>Todo</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
